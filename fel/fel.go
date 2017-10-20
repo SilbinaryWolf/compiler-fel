e4060c487ac4d3d96e68e7f440e4f1fea1f14b96
Jake Bentvelzen <doogie1012@gmail.com>
2017-10-20 19:06:23 +1100

    Add Goja test in

--- fel/fel.go
+++ fel/fel.go
@@ -23,6 +23,21 @@ func main() {
 		log.Printf("Binomial took %s", elapsed)
 	}*/
 
+	/*{
+		// Does not support `let` keyword, ECMAScript 5.1+ only, not ECMAScript 2015.
+		vm := goja.New()
+		v, err := vm.RunScript("filename_here", `
+			var result = 1
+			result += 2
+		`)
+		if err != nil {
+			panic(err)
+		}
+		num := v.Export().(int64)
+		fmt.Printf("Result is: %d\n", num)
+		panic("DONE!")
+	}*/
+
 	program := evaluator.New()
 	err := program.RunProject("../testdata/sampleproject/fel")
 	if err != nil {
